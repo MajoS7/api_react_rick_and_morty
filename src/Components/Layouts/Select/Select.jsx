a866484c1edd451d2ffb62ab7c988f40609d42db
MajoS7 <85207019+MajoS7@users.noreply.github.com>
2021-12-06 10:49:27 -0500

    Primera parte de la funcionalidad select

--- src/Components/Layouts/Select/Select.jsx
+++ src/Components/Layouts/Select/Select.jsx
@@ -1,10 +1,25 @@
+import React, { useEffect, useState } from 'react';
 import './Select.css';
 import { SelectUI } from "../../UI/SelectUI/SelectUI";
 
+
 export const Select =()=>{
+
+    const [element, setElement] = useState([{name:'Select a name',image:''}]);
+    const URL = 'https://rickandmortyapi.com/api/character/';
+
+    const FetchApi=()=> { 
+        fetch(URL)
+        .then(response=>response.json())
+        .then(data=>setElement(data.results))
+    }
+    useEffect(() =>{
+        FetchApi('')
+    },[])
+
     return(
         <div className="Select">
-            < SelectUI />
+            <SelectUI prueba={element}/>
         </div>
     );
 }
--- src/Components/UI/SelectUI/SelectUI.jsx
+++ src/Components/UI/SelectUI/SelectUI.jsx
@@ -1,9 +1,25 @@
 import './SelectUI.css';
+import React, { useEffect } from 'react';
 
-export const SelectUI =()=>{
+
+export const SelectUI =(prueba)=>{
+
+    
+    console.log(prueba);
+    
+    // const llenar=()=>{
+    //     malp.map(item=>{
+    //         console.log(item.name);
+    //         return item.name;
+    //     })
+    // }
+    // useEffect(() =>{
+    //     llenar('')
+    // },[])
+
     return(
-        <select className="SelectUI">
-            <option >Select a name</option>
+        <select  className="SelectUI">
+            <option></option>
             <option >All Cards</option>
         </select>
     );
