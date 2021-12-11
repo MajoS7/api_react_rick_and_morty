84c5869448ba5f5915b4d7b8c3703f1e385d980d
MajoS7 <85207019+MajoS7@users.noreply.github.com>
2021-12-10 21:40:08 -0500

    Se organizo el diseño inicial de la pagina

--- src/Components/Layouts/AllCards/AllCards.jsx
+++ src/Components/Layouts/AllCards/AllCards.jsx
@@ -2,7 +2,7 @@ import { Card } from "../../UI/Card/Card";
 import './AllCards.css';
 
 export const AllCards =({oneObjectResult})=>{
-
+    console.log(oneObjectResult.name);
     if (oneObjectResult.length==20){
         return(
             <div className="AllCards">
@@ -13,9 +13,16 @@ export const AllCards =({oneObjectResult})=>{
             </div>
         ); 
     }
-    return(
-        <div className="AllCards">
-            <Card objectCard={oneObjectResult}/>
-        </div>
-    );
+    else if(oneObjectResult.name=="Select a Name"){
+        return(<div className="highInitial"></div>);
+    }
+    
+    else{
+        return(
+            <div className="AllCards">
+                <Card objectCard={oneObjectResult}/>
+            </div>
+        );
+    }
+    
 }
